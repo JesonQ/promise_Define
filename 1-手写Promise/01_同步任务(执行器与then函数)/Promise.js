d9385937f7ff9d5bf64a9e5df1ee9be91563989e
“gihzq <614977099@qq.com>
2020-07-18 11:55:18 +0800

    ll

--- 1-手写Promise/01_同步任务(执行器与then函数)/Promise.js
+++ 1-手写Promise/01_同步任务(执行器与then函数)/Promise.js
@@ -1,1 +1,14 @@
-// 
+// promise自定义  是一个构造函数
+
+function Promise(excutor) {
+  // 定义resolve
+  function resolve() {
+    
+  }
+  // 定义reject
+  function reject() {
+
+  }
+  // 调用执行器
+  excutor(resolve, reject)
+}
