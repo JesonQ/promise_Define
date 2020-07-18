5c856c9c217a79cde5723dc087d393371ce46f4a
“gihzq <614977099@qq.com>
2020-07-18 14:05:26 +0800

    ll

--- 1-手写Promise/01_同步任务(执行器与then函数)/Promise.js
+++ 1-手写Promise/01_同步任务(执行器与then函数)/Promise.js
@@ -1,14 +1,25 @@
 // promise自定义  是一个构造函数
+console.log("ours")
 
 function Promise(excutor) {
+  // 存储this
+  let self = this;
+  // 定义初始状态
+  self.status = "pending"
   // 定义resolve
   function resolve() {
-    
+    // 修改状态
+    self.status = "resolved"
   }
   // 定义reject
   function reject() {
-
+    // 修改状态
+    self.status = "rejected"
   }
-  // 调用执行器
-  excutor(resolve, reject)
+  try {
+    // 调用执行器
+    excutor(resolve, reject)
+  } catch (e) {
+    reject(e)
+  }
 }
