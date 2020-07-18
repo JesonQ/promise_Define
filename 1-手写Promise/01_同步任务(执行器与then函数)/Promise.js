8e7aa0cd46259e1a3120602ae668f1e11bbe53c8
“gihzq <614977099@qq.com>
2020-07-18 14:25:25 +0800

    data

--- 1-手写Promise/01_同步任务(执行器与then函数)/Promise.js
+++ 1-手写Promise/01_同步任务(执行器与then函数)/Promise.js
@@ -6,20 +6,32 @@ function Promise(excutor) {
   let self = this;
   // 定义初始状态
   self.status = "pending"
+  // 定义数据
+  self.data = ""
   // 定义resolve
-  function resolve() {
+  function resolve(value) {
     // 修改状态
     self.status = "resolved"
+    // 修改数据
+    self.data = value
   }
   // 定义reject
-  function reject() {
+  function reject(reason) {
     // 修改状态
     self.status = "rejected"
+    // 修改数据
+    self.data = reason
   }
+  // 如果执行器抛出错误,状态为rejected
   try {
     // 调用执行器
     excutor(resolve, reject)
   } catch (e) {
     reject(e)
   }
+}
+
+// then方法
+Promise.prototype.then = function (onResolved, onRejected){
+  
 }
