c5da3db1c0e985f468dae573ebb04beab17359f5
“gihzq <614977099@qq.com>
2020-07-18 15:20:36 +0800

    异步

--- 1-手写Promise/02_异步任务实现_保存成功和失败回调函数/Promise.js
+++ 1-手写Promise/02_异步任务实现_保存成功和失败回调函数/Promise.js
@@ -8,12 +8,16 @@ function Promise(excutor) {
   self.status = "pending"
   // 定义数据
   self.data = ""
+  // 存储回调
+  self.callbacks = []
   // 定义resolve
   function resolve(value) {
     // 修改状态
     self.status = "resolved"
     // 修改数据
     self.data = value
+    // 调用成功的回调
+    self.callbacks[0].success(self.data)
   }
   // 定义reject
   function reject(reason) {
@@ -21,6 +25,8 @@ function Promise(excutor) {
     self.status = "rejected"
     // 修改数据
     self.data = reason
+    // 调用失败的回调
+    self.callbacks[0].fail(self.data)
   }
   // 如果执行器抛出错误,状态为rejected
   try {
@@ -34,7 +40,7 @@ function Promise(excutor) {
 // then方法  onResolved   当....的时候...
 Promise.prototype.then = function (onResolved, onRejected){
   // 存储this
-  // let self = this
+  let self = this
   // 当状态为resolved
   if(self.status === "resolved"){
     onResolved(self.data)
@@ -42,5 +48,12 @@ Promise.prototype.then = function (onResolved, onRejected){
   // 当状态为rejected
   if(self.status === "rejected"){
     onRejected(self.data)
+  }
+  // 异步任务 存储回调
+  if(self.status === "pending"){
+    self.callbacks.push({
+      success:onResolved,
+      fail:onRejected
+    })
   }
 }
