dbf0f9148dfcfa29214f1d88188472497fac752c
“gihzq <614977099@qq.com>
2020-07-21 15:13:27 +0800

    a

--- 2-async和await/3-async与await结合.js
+++ 2-async和await/3-async与await结合.js
@@ -1,19 +1,44 @@
 //读取 1.html 和 2.html 两个文件内容, 然后合并后输出到控制台
 
-const fs = require("fs");
-const util = require("util");
-const mineReadFile = util.promisify(fs.readFile);
+let fs = require("fs")
+let util = require("util")
+let mainReadFile = util.promisify(fs.readFile)
 
+// fs读取  回调地狱
+// fs.readFile("./resource/1.html", function (err,data){
+//     fs.readFile("./resource/2.html", function (err,data2){
+//         fs.readFile("./resource/3.html", function (err,data3){
+//             console.log((data+data2+data3).toString())
+//         })
+//     })
+// })
 
-// async 与 await
+// fs读取 Promise
+// new Promise((resolve, reject)=>{
+//     fs.readFile("./resource/1.html", function (err,data){
+//         resolve(data.toString())
+//     })
+// })
+// .then(
+//     (v)=>{
+//         return new Promise((resolve, reject)=>{
+//             fs.readFile("./resource/2.html", function (err,data){
+//                 resolve((v+data).toString())
+//             })
+//         }) 
+//     }
+// )
+// .then(
+//     v=>{
+//         console.log(v)
+//     }
+// )
+
+// async与await
 async function main(){
-    //读取第一个文件的内容
-    let one = await mineReadFile(__dirname+"/resource/1.html");
-    //读取第二个文件的内容
-    let two = await mineReadFile(__dirname+"/resource/2.html");
-    //读取第三个文件的内容
-    let three = await mineReadFile(__dirname+"/resource/3.html");
-    console.log(one + two + three);
+   let one = await mainReadFile("./resource/1.html");
+   let two = await mainReadFile("./resource/2.html");
+   let three = await mainReadFile("./resource/3.html");
+   console.log(one+two+three+"")
 }
-
-main();
+main()
